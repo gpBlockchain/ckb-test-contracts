dce4478a7314e0c2dc8e45ed0c16d0d2c01598b9
gpBlockchain <744158715@qq.com>
2024-05-20 16:54:08 +0800

    remove ckb-c-stdlib

--- prepare.sh
+++ prepare.sh
@@ -4,11 +4,11 @@ cargo install ckb-capsule --git https://github.com/gpBlockchain/capsule.git --br
 echo "build rust contract"
 cd rust/acceptance-contracts
 capsule build
-echo "build c contract "
-cd ../../
-cd c
-mkdir deps
-cd deps
-git clone https://github.com/nervosnetwork/ckb-c-stdlib
-cd ../
-make all-in-docker
+#echo "build c contract "
+#cd ../../
+#cd c
+#mkdir deps
+#cd deps
+#git clone https://github.com/nervosnetwork/ckb-c-stdlib
+#cd ../
+#make all-in-docker
