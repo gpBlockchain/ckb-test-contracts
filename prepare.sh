219fa05fd2c4fa38cb2516599565120864144561
iu小迷弟 <15168316096@163.com>
2024-05-21 06:49:29 +0000

    update rust stable

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 echo "get capsule "
-rustup update
+rustup update stable
 cargo install cross --git https://github.com/cross-rs/cross
 cargo install ckb-capsule --git https://github.com/nervosnetwork/capsule.git --branch develop
 echo "build rust contract"
