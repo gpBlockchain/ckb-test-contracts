f38ac6e93f9f7910235193bb685069512f44004b
gpBlockchain <744158715@qq.com>
2024-05-21 16:21:41 +0800

    add test

--- rust/acceptance-contracts/tests/src/test_ckb_pipe.rs
+++ rust/acceptance-contracts/tests/src/test_ckb_pipe.rs
@@ -2,9 +2,13 @@
 
 
 #[test]
-fn test_read_write_max_data(){}
+fn test_read_write_max_data(){
+    // xyl
+    // 一次性读写比较大的buffer
+}
 
 #[test]
 fn test_pipe_close_when_spawn_stop(){
-
+    // xyl
+    // 进程结束后，进程拥有的fd自动关闭
 }
--- rust/acceptance-contracts/tests/src/test_ckb_process_id.rs
+++ rust/acceptance-contracts/tests/src/test_ckb_process_id.rs
@@ -1,4 +1,7 @@
 
 
 #[test]
-fn test_ckb_process_id_in_exec(){}
+fn test_ckb_process_id_in_exec(){
+    // gp
+    // 在exec 调用 ckb_process_id
+}
