36d663eb6f49a63f9da61c02559aa43eb940cd60
gpBlockchain <744158715@qq.com>
2024-06-06 09:19:23 +0800

    fix: print_current_cycle death code

--- rust/acceptance-contracts/contracts/exec/src/exec_with_exec.rs
+++ rust/acceptance-contracts/contracts/exec/src/exec_with_exec.rs
@@ -15,7 +15,6 @@ default_alloc!();
 
 use ckb_std::{syscalls};
 use ckb_std::ckb_constants::Source;
-use ckb_std::syscalls::{current_cycles, debug};
 
 /// 1. invoke -> spawn(1) -> exec(1)
 /// 2. exec(1) -> spawn(2)
@@ -26,9 +25,4 @@ pub fn program_entry() -> i8 {
     }
     syscalls::exec(0, Source::CellDep, 0, 0, &[]);
     return 0;
-}
-
-fn print_current_cycle() {
-    let pid = syscalls::process_id();
-    debug(format!("id:{:?},cycle:{:?}", pid, current_cycles()))
 }
